9dd7d1bea213f0cb63a5609a65cb9fb29239aeb3
Vuong Nguyen <yeungon@gmail.com>
2019-09-30 05:15:52 -0600

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app">
+   <div class="wrapper clearfix">
+            <players/>
+            <controls/>
+            <dices/>          
+            
+            
+    </div>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import Players from "@/components/Players";
+import Controls from "@/components/Controls";
+import Dices from "@/components/Dices";
 
 export default {
   name: 'app',
+  data: function(){
+    return {
+      
+      }
+  },
   components: {
-    HelloWorld
-  }
+   Players,
+   Controls,
+   Dices
+  },
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    
 }
+
+.clearfix::after {
+    content: "";
+    display: table;
+    clear: both;
+}
+
+body {
+    background-image: linear-gradient(rgba(62, 20, 20, 0.4), rgba(62, 20, 20, 0.4)), url("assets/back.jpg");
+    background-size: cover;
+    background-position: center;
+    font-family: Lato;
+    font-weight: 300;
+    position: relative;
+    height: 100vh;
+    color: #555;
+}
+
+.wrapper {
+    width: 1000px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.3);
+    overflow: hidden;
+}
+
+
 </style>
